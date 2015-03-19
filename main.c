0d04de2af2334e509c4baa19c63f39cdaff3fbb5
240129745 <240129745@qq.com>
2015-03-19 12:04:58 +0800

    test over buff

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
+#include<stdlib.h>
+void testfun(char *buf)
+{
+	char str[10]={0};
+	strcpy(str,buf);
+	printf("%s",str);
+}
 int main()
 {
 	int i=0;
 	for(i=0;i<9;i++)
 		printf("%d\n",i+1+3);
-	return NULL;
+	testfun("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	return 1;
 }
